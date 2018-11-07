93190878f6200c29fde9df92ab48ebcc00a06a14
zyt <sunshine7034520@gmail.com>
2018-11-08 04:12:40 +0800

    beforeonline

---
+++ src/page/about/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+require('page/common/header/index.js');
+var nav = require('page/common/nav/index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ var getHtmlConfig = function(name, title){
     return {
         template    : './src/view/' + name + '.html',
         filename    : 'view/' + name + '.html',
+        favicon     : './favicon.ico',
         title       : title,
         inject      : true,
         hash        : true,
@@ -34,11 +35,12 @@ var config = {
         'user-center'       : ['./src/page/user-center/index.js'],
         'user-center-update': ['./src/page/user-center-update/index.js'],
         'user-pass-update'  : ['./src/page/user-pass-update/index.js'],
-        'result'            : ['./src/page/result/index.js']
+        'result'            : ['./src/page/result/index.js'],
+        'about'            : ['./src/page/about/index.js']
     },
     output: {
-        path: './dist',
-        publicPath : '/dist',
+        path: __dirname + '/dist/',
+        publicPath : 'dev' === WEBPACK_ENV ? '/dist/' : '//s.lovetao.ml/mmall-fe/dist/',
         filename: 'js/[name].js'
     },
     externals : {
@@ -55,7 +57,14 @@ var config = {
                     name: 'resource/[name].[ext]'
                 }
             },
-            { test: /\.string$/, loader: 'html-loader'}
+            {
+                test: /\.string$/,
+                loader: 'html-loader',
+                query : {
+                    minimize : true,
+                    removeAttributeQuotes : false
+                }
+            }
         ]
     },
     resolve : {
@@ -90,7 +99,8 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
-        new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果'))
+        new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMall'))
     ]
 };
 
